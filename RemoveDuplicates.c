e86cccb17742f52276620904a3c1fd4056d00ad8
Vishal Ravishankar <68605177+vishal-git21@users.noreply.github.com>
2024-07-04 16:19:17 +0530

    Update RemoveDuplicates.c

--- RemoveDuplicates.c
+++ RemoveDuplicates.c
@@ -1,4 +1,4 @@
-''' Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+/* Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
 
 Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
 
@@ -41,7 +41,7 @@ Constraints:
 -100 <= nums[i] <= 100
 nums is sorted in non-decreasing order.
 
-'''
+*/
 
 
 int removeDuplicates(int* nums, int numsSize) {
