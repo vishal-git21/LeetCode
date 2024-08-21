382e38a282e1523cb13c60d7109dc1ffa214c043
Vishal Ravishankar <68605177+vishal-git21@users.noreply.github.com>
2024-08-21 22:51:21 +0530

    Add files via upload

--- SpiralMatrix.py
+++ SpiralMatrix.py
@@ -1,48 +1,40 @@
-# Given an m x n matrix, return all elements of the matrix in spiral order.
-
-
-from typing import List
-
 class Solution:
-    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        if not matrix:
-            return []
+    def generateMatrix(self, n: int) -> List[List[int]]:
+        matrix = []
+        for i in range(n):
+            matrix.append([])
+        for j in range(n):
+            for k in range(n):
+                matrix[j].append(0)
+        i = 1
+        x,y = 0,0
+        d = "right"
+        while i <= n * n:
+            matrix[x][y] = i  
+            i += 1
+            if d == "right":
+                if y + 1 < n and matrix[x][y + 1] == 0:
+                    y += 1
+                else:
+                    d = "down"
+                    x += 1
+            elif d == "down":
+                if x + 1 < n and matrix[x + 1][y] == 0:
+                    x += 1
+                else:
+                    d = "left"
+                    y -= 1
+            elif d == "left":
+                if y - 1 >= 0 and matrix[x][y - 1] == 0:
+                    y -= 1
+                else:
+                    d = "up"
+                    x -= 1
+            elif d == "up":
+                if x - 1 >= 0 and matrix[x - 1][y] == 0:
+                    x -= 1
+                else:
+                    d = "right"
+                    y += 1
         
-        l = []
-        m = len(matrix)
-        n = len(matrix[0])
-        i, j = 0, 0
-        arrow = "right"
-        
-        while len(l) < m * n:
-            l.append(matrix[i][j])
-            matrix[i][j] = None  
-            
-            if arrow == "right":
-                if j + 1 < n and matrix[i][j + 1] is not None:
-                    j += 1
-                else:
-                    arrow = "down"
-                    i += 1
-            elif arrow == "down":
-                if i + 1 < m and matrix[i + 1][j] is not None:
-                    i += 1
-                else:
-                    arrow = "left"
-                    j -= 1
-            elif arrow == "left":
-                if j - 1 >= 0 and matrix[i][j - 1] is not None:
-                    j -= 1
-                else:
-                    arrow = "up"
-                    i -= 1
-            elif arrow == "up":
-                if i - 1 >= 0 and matrix[i - 1][j] is not None:
-                    i -= 1
-                else:
-                    arrow = "right"
-                    j += 1
-        
-        return l
-
-
+        return matrix
